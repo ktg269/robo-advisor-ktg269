f0b4e094ebbd3c7eba310d895bc5334f1044ac2c
ktg269 <50781186+ktg269@users.noreply.github.com>
2019-06-18 23:01:40 -0400

    Issue a request

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,5 +1,30 @@
 # app/robo_advisor.py
 
+import requests
+import json
+
+#
+# INFO INPUTS
+#
+
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=demo"
+
+response = requests.get(request_url)
+# print(type(response)) #> <class 'requests.models.Response'>
+# print(response.status_code) #> 200
+# print(response.text)
+
+parsed_response = json.loads(response.text)
+
+breakpoint()
+
+
+#
+# INFO OUTPUTS
+#
+
+
+
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")

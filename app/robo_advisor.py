188aa3edfc702f9750971ec7b45c3da493d66b2e
ktg269 <50781186+ktg269@users.noreply.github.com>
2019-06-19 02:24:53 -0400

    Get latest close

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -3,11 +3,17 @@
 import requests
 import json
 
+# utility function to convert float or integer to usd-formatted string (for printing)
+
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price) #> 12,000.71
+
+
 #
 # INFO INPUTS
 #
 
-request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&outputsize=full&apikey=demo"
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
 
 response = requests.get(request_url)
 # print(type(response)) #> <class 'requests.models.Response'>
@@ -18,7 +24,7 @@ parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
-
+latest_close = parsed_response["Time Series (Daily)"]["2019-06-18"]["4. close"]
 #breakpoint()
 
 
@@ -35,7 +41,7 @@ print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")

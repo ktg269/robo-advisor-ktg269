20ad138b0a7036818add3b076c8b51821743cf94
ktg269 <50781186+ktg269@users.noreply.github.com>
2019-06-20 23:46:49 -0400

    Dynamically get latest day's closing price

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -16,17 +16,20 @@ def to_usd(my_price):
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
 
 response = requests.get(request_url)
-# print(type(response)) #> <class 'requests.models.Response'>
-# print(response.status_code) #> 200
-# print(response.text)
 
 parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
-latest_close = parsed_response["Time Series (Daily)"]["2019-06-18"]["4. close"]
 #breakpoint()
 
+tsd = parsed_response["Time Series (Daily)"]
+
+dates = list(tsd.keys()) #TODO: assumes first day is on top, but consider sorting to ensure latest day is first
+
+latest_day = dates[0] #TODO: make the latest date dynamic
+
+latest_close = tsd[latest_day]["4. close"]
 
 #
 # INFO OUTPUTS
